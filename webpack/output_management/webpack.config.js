ff38d557148f666e60eed04d74e8141e7aee06dd
DylanB <dylanbergmann25@gmail.com>
2023-08-03 08:01:03 +0700

    added webpack tutorials and 3 rails projects

---
+++ webpack/output_management/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+  // entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ // npm install --save-dev html-webpack-plugin
+      title: 'Output Management',
+    }),
+  ],
+  output: {
+    // filename: 'main.js',
+    // filename: 'bundle.js',
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
